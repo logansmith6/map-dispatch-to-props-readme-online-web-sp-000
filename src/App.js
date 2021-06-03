4906d75fa415c3fcddee7abc44796680474fb502
Logan Smith <logsmith06@gmail.com>
2021-06-03 19:37:28 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  handleOnClick = event => {
+    this.props.addItem() // Code change: this.props.store.dispatch is no longer being called
   }
 
   render() {
